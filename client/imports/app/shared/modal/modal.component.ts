d9b2cea44eb45529d59d744b7828cad2c5f2b1f6
jnncks <jannickscherf@gmx.de>
2017-04-10 14:11:23 +0200

    Improve the modal style and add animations

--- client/imports/app/shared/modal/modal.component.ts
+++ client/imports/app/shared/modal/modal.component.ts
@@ -1,14 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/core';
 
 import { ModalService } from './modal.service';
 
 import template from './modal.component.html';
-import style from './modal.component.scss';
+import styleUrl from './modal.component.scss';
 
 @Component({
   selector: 'modal',
   template,
-  styles: [ style ],
+  styles: [ styleUrl ],
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({opacity: '1'})),
+      transition(':enter', [
+        style({opacity: '0'}),
+        animate(125)
+      ]),
+      transition(':leave', [
+        animate(125, style({opacity: '0'}))
+      ])
+    ])
+  ],
   host: { '(document:keyup)': 'keyup($event)' }
 })
 export class Modal implements OnInit {
